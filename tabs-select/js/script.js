69fed1d72b798412652565c8a1a09991a1a50c1d
Dmitriy <proteinr@gmail.com>
2018-10-09 09:55:29 +0300

    Dropdown

--- tabs-select/js/script.js
+++ tabs-select/js/script.js
@@ -1,1 +1,11 @@
-$('h1').addClass('active');
+$('.button-dropdown').click(function () {
+    //    alert('click');
+    $('ul').toggleClass('active');
+})
+
+
+$('.tabs li').click(function () {
+    var target = $(this).attr('data-tab');
+    $('.tabs')
+    //    alert(target);
+})
