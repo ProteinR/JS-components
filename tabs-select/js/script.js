57cfebf47117a00b174f03b2a6712c512b38d7ae
Dmitriy <proteinr@gmail.com>
2018-10-09 21:31:52 +0300

    tabs works

--- tabs-select/js/script.js
+++ tabs-select/js/script.js
@@ -1,11 +1,22 @@
-$('.button-dropdown').click(function () {
+$('.button-dropdown').on('click', function () {
     //    alert('click');
     $('ul').toggleClass('active');
-})
+});
+
+$('.button-dropdown').blur(function () {
+    alert('blur');
+    $('ul').removeClass('active');
+});
 
 
+
+//tabs
 $('.tabs li').click(function () {
-    var target = $(this).attr('data-tab');
-    $('.tabs')
-    //    alert(target);
-})
+    var target = $(this).attr('data-tab'); //достали номер вкладки
+    console.log(target);
+    $('li').removeClass(); //убрали подсветку у всех вкладок
+    $(this).addClass('active-tab'); //подсветили вкладку на которую нажали
+
+    $('.tab-container .content p').removeClass('active-p');
+    $(target).addClass('active-p'); //выбрали абзац с id = target
+});
