3185b5373700608da502adc2cc9a27f83dcdbb1a
Dmitriy <proteinr@gmail.com>
2018-10-12 23:58:26 +0300

    Fullscreen slider

--- slider/js/main.js
+++ slider/js/main.js
@@ -1,9 +1,30 @@
+var divWidth = $(".slider-container").width(); //получаем ширину блока
+var numImg = 0; //номер листаемого изображения
+var imgCount = $('.slide').length; //кол-во изображений в слайдере
+var maxWidth = imgCount * divWidth; // ширина блока с учетом всех картинок
+
+//var length = 0; //- высчитывает на сколько прокрутить ленту 
+
+console.log('Ширина блока - ' + divWidth + 'px');
+
+
 $('.arrow.arrow-left').click(function () {
-    alert('Left');
+    numImg--;
+    if (numImg == -1) {
+        numImg = imgCount;
+        console.log('numImg = ' + numImg);
+
+    }
+    $('.slide-row').css('left', divWidth * numImg * (-1));
 });
 
 
-
 $('.arrow.arrow-right').click(function () {
-    alert('Right');
+    numImg++;
+    if (numImg < imgCount) {
+        console.log(numImg);
+    } else {
+        numImg = 0;
+    }
+    $('.slide-row').css('left', divWidth * numImg * (-1));
 });
