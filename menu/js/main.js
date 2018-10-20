55f54e8894980b978360b97fa921f950df453121
Dmitriy <proteinr@gmail.com>
2018-10-20 20:59:05 +0300

    Hamburger menu

---
+++ menu/js/main.js
@@ -0,0 +1,12 @@
+$('.btn-menu').click(function () {
+    $('.menu').toggleClass('active');
+    $('.dark-block').toggleClass('active');
+});
+
+
+
+$('.dark-block').click(function () {
+    $('.menu').toggleClass('active');
+    $('.dark-block').toggleClass('active');
+
+});
