27e15d627b2c374786d8ca16ba741fe271b87caa
Dmitriy <proteinr@gmail.com>
2018-11-02 01:04:25 +0200

    Ver 0.1

--- before-after-img/js/main.js
+++ before-after-img/js/main.js
@@ -1,24 +1,32 @@
 var x0, x1, distance = 0;
+var distance_procent; //расстояние в процентах от блока
 var flag = false; //нажата ли левая клавиша мыши
+var box_width = parseInt($('.before-after-wrapper').css('width')); //ширина контейнера
+var line_pos = parseInt($('.line').css('left')) * 100 / box_width; //позиция полосы в процентах
+var left_img_width = parseInt($('.left-img').css('width')) * 100 / box_width; //шир левого изобр в процентах
+//console.log(left_img_width);
 
 
 $('.before-after-wrapper').mousedown(function (e) {
     flag = true;
     x0 = e.pageX;
-    //    alert('click');
+    //    line_pos = parseInt($('.line').css('left'));
+    //    console.log('left у line ' + line_pos + '(%)');
 
-    //    alert(flag);
+
     $(this).mousemove(function (e) {
         if (flag) {
             x1 = e.pageX;
             distance = x1 - x0;
-            $('.line').css('left', +distance);
-            $('.left-img').css('width', +distance);
+
+            distance_procent = distance * 100 / box_width;
+            console.log(distance_procent);
+            $('.left-img').css('width', left_img_width + distance_procent + '%');
+            $('.line').css('left', line_pos + distance_procent + '%');
         }
     })
 });
 
 $('.before-after-wrapper').mouseup(function () {
     flag = false;
-    console.log(flag);
 })
