0b49d88d6f3a5d401570cc92fee61a7a7aa11922
Dmitriy <proteinr@gmail.com>
2018-11-02 01:27:55 +0200

    Bug fixed

--- before-after-img/js/main.js
+++ before-after-img/js/main.js
@@ -2,17 +2,15 @@ var x0, x1, distance = 0;
 var distance_procent; //расстояние в процентах от блока
 var flag = false; //нажата ли левая клавиша мыши
 var box_width = parseInt($('.before-after-wrapper').css('width')); //ширина контейнера
-var line_pos = parseInt($('.line').css('left')) * 100 / box_width; //позиция полосы в процентах
-var left_img_width = parseInt($('.left-img').css('width')) * 100 / box_width; //шир левого изобр в процентах
+var line_pos; //позиция полосы в процентах
+var left_img_width; //шир левого изобр в процентах
 //console.log(left_img_width);
-
 
 $('.before-after-wrapper').mousedown(function (e) {
     flag = true;
     x0 = e.pageX;
-    //    line_pos = parseInt($('.line').css('left'));
-    //    console.log('left у line ' + line_pos + '(%)');
-
+    line_pos = parseInt($('.line').css('left')) * 100 / box_width;
+    left_img_width = parseInt($('.left-img').css('width')) * 100 / box_width;
 
     $(this).mousemove(function (e) {
         if (flag) {
@@ -21,8 +19,10 @@ $('.before-after-wrapper').mousedown(function (e) {
 
             distance_procent = distance * 100 / box_width;
             console.log(distance_procent);
-            $('.left-img').css('width', left_img_width + distance_procent + '%');
-            $('.line').css('left', line_pos + distance_procent + '%');
+            if (!(distance_procent <= -49 || distance_procent >= 51)) {
+                $('.left-img').css('width', left_img_width + distance_procent + '%');
+                $('.line').css('left', line_pos + distance_procent + '%');
+            }
         }
     })
 });
