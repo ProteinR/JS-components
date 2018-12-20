fc4d46d4381fd59edbe3332ab47c6aa5371c5776
Dmitriy <proteinr@gmail.com>
2018-12-20 23:52:24 +0200

    Bug fixed

--- before-after-img/js/compare.js
+++ before-after-img/js/compare.js
@@ -14,13 +14,27 @@ $.fn.compare = function (options) {
     console.log(box_width);
     var line_pos; //позиция полосы в процентах
     var left_img_width; //шир левого изобр в процентах
+    var right_border_distance;
 
     //controllers
+    function compareController() {
+        $selector.find('.left-img').css('width', left_img_width + distance_procent + '%');
+        $selector.find('.line').css('left', line_pos + distance_procent + '%');
+    }
 
 
     //init
     $selector.find('.left-img').css('width', params.percent + '%'); //задаём ширину левого(верхнего изображения)
     $selector.find('.line').css('left', -100 + params.percent + '%');
+    $selector.find('.left-img > img').css('width', box_width);
+
+    if (params.arrows == true) {
+        $selector.find('.line').addClass('line-true');
+        console.log('arrow true');
+    } else {
+        $selector.find('.line').addClass('line-false');
+        console.log('arrow false');
+    }
 
     //action
     $selector.find('.line').mousedown(function (e) {
@@ -28,10 +42,12 @@ $.fn.compare = function (options) {
         x0 = e.pageX;
         line_pos = parseInt($('.line').css('left')) * 100 / box_width;
         left_img_width = parseInt($selector.find('.left-img').css('width')) * 100 / box_width;
-        console.log(line_pos);
+        right_border_distance = 100 - left_img_width;
+
+        console.log(right_border_distance);
     });
 
-    $selector.mouseup(function (e) {
+    $(document).mouseup(function (e) {
         flag = false;
     });
 
@@ -42,18 +58,11 @@ $.fn.compare = function (options) {
             distance_procent = distance * 100 / box_width;
             console.log(distance_procent);
 
-            if (!(distance_procent <= -49 || distance_procent >= 51)) {
-                $selector.find('.left-img').css('width', left_img_width + distance_procent + '%');
-                $selector.find('.line').css('left', line_pos + distance_procent + '%');
-            }
+            if (!(distance_procent <= -left_img_width || distance_procent > right_border_distance)) {
+                console.log($selector.find('.left-img').css('width' + 'perc'));
 
+                compareController();
+            }
         }
     })
-
-
-
-
-
-
-
 }
